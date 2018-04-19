59ba10e64ecdf5354038162f6381f5963c927b05
atexide fabrice <navyblu971@hotmail.com>
2018-04-19 12:10:39 +0200

    just creating finance3.py ...

---
+++ finance3.py
@@ -0,0 +1,11 @@
+def load_data(y_name='Species'):
+    """Returns the iris dataset as (train_x, train_y), (test_x, test_y)."""
+    train_path, test_path = maybe_download()
+
+    train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+    train_x, train_y = train, train.pop(y_name)
+
+    test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+    test_x, test_y = test, test.pop(y_name)
+
+    return (train_x, train_y), (test_x, test_y)
