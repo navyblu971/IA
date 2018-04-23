132c4c4ca72213876d2fbd3266276f7a0b6f28e3
atexide fabrice <navyblu971@hotmail.com>
2018-04-23 20:52:47 +0200

    sav..

--- finance2.py
+++ finance2.py
@@ -9,17 +9,22 @@ import tensorflow as tf
 
 
 
+def backspace(n):
+    # print((b'\x08').decode(), end='')     # use \x08 char to go back
+    print('\r', end='')                     # use '\r' to go back
+
+
 
 def getData(y_name='749352'):
 	file_name = "../cours.csv"
 
 	#columns_list = pd.read_csv(file_name, nrows=1).columns
-	data = pd.read_csv(file_name, sep=';',   dtype=str, decimal=',' )
+	data = pd.read_csv(file_name, sep=';',   dtype=str, decimal=',' , usecols=[20])
 	pd.fillna(0)
 
 	data_x, data_y = data, data.pop(y_name)
 
-	columns_list = pd.read_csv(file_name, nrows=0, sep=';').columns.values.tolist()
+	columns_list = pd.read_csv(file_name, nrows=0, sep=';', usecols=[20]).columns.values.tolist()
 
 	print (columns_list)
 
@@ -41,6 +46,19 @@ def getData(y_name='749352'):
 	return data_x, data_y, columns_list
 
 	##print (load_data())
+
+
+
+def _parse_line(COLUMNS_NAME,column_number ,  line):
+    # Decode the line into its fields
+
+
+    # Pack the result into a dictionary
+    features = dict(zip(COLUMNS_NAME,line))
+	#label = features[column_number]
+    return features, label
+
+
 
 
 def train_input_fn(features, labels, batch_size):
@@ -85,25 +103,25 @@ def map_float(x):
 #y = numpy.zeros((2250,254),dtype=numpy.float64)
 
 
-def getFeaturesAndLabel (file_name = "../cours.csv" , y_name='749352') :
-	'''return features and y as column_number '''
+def getFeaturesAndLabel (file_name = "../cours.csv" , y_name='749352' ) :
+			'''return features and y as column_number '''
 
-			#columns_list = pd.read_csv(file_name, nrows=1).columns
-			data = pd.read_csv(file_name, sep=';',   dtype=str, decimal=',' )
-			pd.fillna(0)
+			NB_COLUMNS = 20
+			data = pd.read_csv(file_name, sep=';',   dtype=str, decimal=',' , nrows=1100 , usecols= range(NB_COLUMNS))
 
 
-			columns_list = pd.read_csv(file_name, nrows=0, sep=';').columns.values.tolist()
 
-			print (columns_list)
+			columns_list = pd.read_csv(file_name, nrows=0, sep=';' , usecols=range (NB_COLUMNS)).columns.values.tolist()
+
+			#print (columns_list)
 
 			''' on enleve le y_name qui est utilisé comme label, ils sont deux fois dans la liste d'ou les deux removes ...'''
 
 			column_number = columns_list.index(y_name)
 
-			columns_list.remove('Identifiant')
-			columns_list.remove(y_name)
-			columns_list.remove(y_name+".1")
+			#columns_list.remove('Identifiant')
+			#columns_list.remove(y_name)
+			#columns_list.remove(y_name+".1")
 
 			number_of_rows = data.shape[0]
 			number_of_columns = data.shape[1]
@@ -112,16 +130,25 @@ def getFeaturesAndLabel (file_name = "../cours.csv" , y_name='749352') :
 			y = [[0 for x in range(w)] for y in range(h)]
 
 
+			features = []
+			#dict(zip(columns_list,[0 for i in columns_list]))
+
+
+			print (features)
+
+
 
 			line =1
+			'''
 			while line < number_of_rows:
-				tmp = list( map (myTransform ,DATA.iloc[line,1:] ) )
+				tmp = list( map (myTransform ,pd.iloc[line,1:] ) )
 				y[line-1][:]= tmp
 				line+=1
+			'''
 
 			#print ("y = " , y[0][:])
 
-
+			'''on garde la premiere ligne de donnée tel quel ..on commence à la ligne 2'''
 			line = 2
 			while (line < number_of_rows):
 				#on ne touche pas à la premiere colonne
@@ -135,6 +162,12 @@ def getFeaturesAndLabel (file_name = "../cours.csv" , y_name='749352') :
 						y[line][col] = 0
 					else:
 						y[line][col] = 1
+
+					features.append( dict(zip(columns_list,y[line])) )
+					print (line)
+					backspace(len(str(line)))
+					#print (features)
+
 		#except:
     			#print("Voici l'erreur :", exception_retournee)
 		#		print("col", col)
@@ -143,8 +176,14 @@ def getFeaturesAndLabel (file_name = "../cours.csv" , y_name='749352') :
 					col+=1
 				line+=1
 
-			return y, y[:][column_number],columns_list ##features
 
+			print (features[2])
+			#return y, y[:][column_number],columns_list ##features
+
+
+			labels = features[column_number]
+			del features[column_number]
+			return features, labels, columns_list
 
 
 
@@ -155,7 +194,7 @@ print ("step 2")
 #features, labels = getFeaturesAndLabel (data , 3)
 print ("step 3")
 #print (features[2248][:])
-print (labels)
+#print (labels)
 print ("step 4")
 
 #train_input_fn(features, labels, 100)
@@ -174,7 +213,7 @@ print ("step 5")
 
 # Build a DNN with 2 hidden layers and 10 nodes in each hidden layer.
 
-
+print ("create classifier ...")
 classifier = tf.estimator.DNNClassifier(
     feature_columns=my_feature_columns,
     # Two hidden layers of 10 nodes each.
@@ -183,6 +222,19 @@ classifier = tf.estimator.DNNClassifier(
     n_classes=3)
 
 
+
+
+
+
+print ("train the model ..")
 # Train the Model.
 classifier.train(
     input_fn=train_input_fn(features, labels, 100), steps=Step)
+
+
+
+eval_result = classifier.evaluate(
+	input_fn=train_input_fn(features, labels,100),  steps=Step)
+
+
+print (eval_result)
